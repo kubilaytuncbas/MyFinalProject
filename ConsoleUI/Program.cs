4afbfbb836313c1655fd14beed1eb7b65230ef5e
kubilaytuncbas <kubilaytuncbas@icloud.com>
2021-03-27 02:26:12 +0300

    9.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,14 +7,31 @@ namespace ConsoleUI
 {
     class Program
     {
+        // Data Transformation Object
         //SOLID
         //Open Closed Principle
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName+ "/" + product.CategoryName);
             }
         }
     }
---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿select Products.ProductName,sum([Order Details].UnitPrice * [Order Details].Quantity) AS 'TOPLAM MİKTAR'
+FROM (([Order Details] inner join Products on [Order Details].ProductID=Products.ProductID) inner join Orders on orders.OrderID=[Order Details].OrderID)
+group by Products.ProductName
